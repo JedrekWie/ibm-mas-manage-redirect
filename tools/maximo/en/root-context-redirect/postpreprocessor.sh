28c33bf1c6603ef96f2a3fef8ca9262fe67faf9c
Andrzej Więcław <andrzej.wieclaw@naviam.io>
2025-12-16 15:20:05 +0100

    Add special support for UI builds (buildmaximoui-war.sh)

--- tools/maximo/en/root-context-redirect/postpreprocessor.sh
+++ tools/maximo/en/root-context-redirect/postpreprocessor.sh
@@ -14,20 +14,23 @@ find "$SEARCH_DIR" -type f \( -name 'maximo-all.sh' -o -name 'maximo-ui.sh' \) |
 
   # Update corresponding server.xml file
   # Comment out the following section if you prefer to register it manually 
-  # in the server bundle's aditional server config.
+  # in the server bundle's additional server config.
   # Ref. https://www.ibm.com/docs/en/mas-cd/maximo-manage/continuous-delivery?topic=customizing-configuring-application-server
   SERVER_FILE="$CONFIG_DIR/server.xml"
   SED_COMMAND="/<\/application>/a\<webApplication name=\"root-context-redirect\" location=\"root-context-redirect.war\" context-root=\"/\" />"
   echo "[CUSTOM] Registering root-context-redirect in '$SERVER_FILE'"
   sed -i "$SED_COMMAND" "$SERVER_FILE"
 
+  # Handle inconsequent UI server bundle builds which are performed using buildmaximoui-war.sh script
+  SH_TARGETS=("$SH_FILE")
+  [ "$SH_NAME" == "maximo-ui" ] && SH_TARGETS+=("$SH_DIR/buildmaximoui-war.sh")
   # Inject the logic of copying redirect WAR file
   # NOTE: This operation cannot be performed at the time when the postpreprocessor.sh hook itself executes because
   #       deployment directory gets cleaned during the EAR build process.
-  echo "[CUSTOM] Registering post-build hook in $SH_FILE"
+  echo "[CUSTOM] Registering post-build hook in: ${SH_TARGETS[*]}"
   {
     echo; echo # Extra line breaks
     echo "echo '[CUSTOM] Installing $REDIRECT_WAR web application...'"
     echo "cp \"$SCRIPT_DIR/$REDIRECT_WAR\" \"$DEPLOYMENT_DIR/apps/\""
-  } >> "$SH_FILE"
+  } | tee -a "${SH_TARGETS[@]}" > /dev/null
 done
